a898d1def685700c5cb3d881c7ea8273f7e6561d
MORENO ROJAS John Jairo <johnmorenoing@gmail.com>
2020-04-04 16:07:28 +0200

    fix ssl acceptance test

--- chatbot-backend-web/src/test/java/co/com/santander/chatbot/backend/web/integration/steps/HttpClient.java
+++ chatbot-backend-web/src/test/java/co/com/santander/chatbot/backend/web/integration/steps/HttpClient.java
@@ -1,5 +1,6 @@
 package co.com.santander.chatbot.backend.web.integration.steps;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -10,12 +11,13 @@ import static io.cucumber.spring.CucumberTestContext.SCOPE_CUCUMBER_GLUE;
 @Component
 @Scope(SCOPE_CUCUMBER_GLUE)
 public class HttpClient {
-    private final String SERVER_URL = "http://localhost";
+    private final String SERVER_URL = "https://localhost";
     private final String EXAMPLE_ENDPOINT = "/chatbot-backend";
 
     @LocalServerPort
     private int port;
-    private final RestTemplate restTemplate = new RestTemplate();
+    @Autowired
+    private  RestTemplate restTemplate;
 
     private String exampleEndPoint() {
         return SERVER_URL + ":" + port + EXAMPLE_ENDPOINT;
