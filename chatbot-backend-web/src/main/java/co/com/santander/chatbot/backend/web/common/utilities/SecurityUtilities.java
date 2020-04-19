8bf5009b7ea2317d0a85e72be9a9b53eb4c95941
MORENO ROJAS John Jairo <johnmorenoing@gmail.com>
2020-04-19 15:00:55 +0200

    fix algoritm cipher by sonar

--- chatbot-backend-web/src/main/java/co/com/santander/chatbot/backend/web/common/utilities/SecurityUtilities.java
+++ chatbot-backend-web/src/main/java/co/com/santander/chatbot/backend/web/common/utilities/SecurityUtilities.java
@@ -1,55 +1,66 @@
 
 package co.com.santander.chatbot.backend.web.common.utilities;
 
-import org.apache.commons.codec.binary.Base64;
+import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Value;
 
-import javax.crypto.*;
+import javax.crypto.Cipher;
+import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
-import java.io.UnsupportedEncodingException;
-import java.security.InvalidKeyException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
+import java.util.Base64;
 import java.util.Objects;
 
-
+@Log
 public class SecurityUtilities {
 
-    public static final String SEMILLA_DEFAULT = "1qazxsw23EDC";
-    public static final String CIPHER = "DESede";
-    public static final String TEXT_CODE = "utf-8";
-    public static final String MESSAGE_DIGEST = "MD5";
+    public static final int AES_KEY_SIZE = 256;
+    public static final int GCM_IV_LENGTH = 12;
+    public static final int GCM_TAG_LENGTH = 16;
+    private static final byte[] IV = new byte[GCM_IV_LENGTH];
+    public static final String SEMILLA_DEFAULT = "1234567890123456";
+    public static final String CIPHER = "AES/GCM/NoPadding";
+    public static final String CIPHER_SECRET_KEY = "AES";
+
     @Value("${security.semilla}")
     private static String semilla;
 
-    public static String encriptar(String texto) throws UnsupportedEncodingException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+    private SecurityUtilities() {
+    }
+
+    public static String encriptar(String texto) throws Exception {
         if (Objects.isNull(semilla))
             semilla = SEMILLA_DEFAULT;
-        MessageDigest md = MessageDigest.getInstance(MESSAGE_DIGEST);
-        byte[] digestOfPassword = md.digest(semilla.getBytes(TEXT_CODE));
-        byte[] keyBytes = Arrays.copyOf(digestOfPassword, 24);
-        SecretKey key = new SecretKeySpec(keyBytes, CIPHER);
+        // Get Cipher Instance
         Cipher cipher = Cipher.getInstance(CIPHER);
-        cipher.init(Cipher.ENCRYPT_MODE, key);
+        // Create SecretKeySpec
+        SecretKeySpec keySpec = new SecretKeySpec(semilla.getBytes(), CIPHER_SECRET_KEY);
+        // Create GCMParameterSpec
+        GCMParameterSpec gcmParameterSpec = new GCMParameterSpec(GCM_TAG_LENGTH * 8, IV);
+        // Initialize Cipher for ENCRYPT_MODE
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec, gcmParameterSpec);
 
-        byte[] plainTextBytes = texto.getBytes(TEXT_CODE);
-        byte[] buf = cipher.doFinal(plainTextBytes);
-        byte[] base64Bytes = Base64.encodeBase64(buf);
-        return new String(base64Bytes);
+        // Perform Encryption
+        byte[] cipherText = cipher.doFinal(texto.getBytes());
+
+        return Base64.getEncoder().encodeToString(cipherText);
     }
 
-    public static String desencriptar(String texto) throws UnsupportedEncodingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
-        if (Objects.isNull(semilla))
-            semilla = SEMILLA_DEFAULT;
-        byte[] message = Base64.decodeBase64(texto.getBytes(TEXT_CODE));
-        MessageDigest md = MessageDigest.getInstance(MESSAGE_DIGEST);
-        byte[] digestOfPassword = md.digest(semilla.getBytes(TEXT_CODE));
-        byte[] keyBytes = Arrays.copyOf(digestOfPassword, 24);
-        SecretKey key = new SecretKeySpec(keyBytes, CIPHER);
-        Cipher decipher = Cipher.getInstance(CIPHER);
-        decipher.init(Cipher.DECRYPT_MODE, key);
-        byte[] plainText = decipher.doFinal(message);
-        return new String(plainText, "UTF-8");
+    public static String desencriptar(String texto) throws Exception {
+        // Get Cipher Instance
+        Cipher cipher = Cipher.getInstance(CIPHER);
+        // Create SecretKeySpec
+        SecretKeySpec keySpec = new SecretKeySpec(semilla.getBytes(), CIPHER_SECRET_KEY);
+
+        // Create GCMParameterSpec
+        GCMParameterSpec gcmParameterSpec = new GCMParameterSpec(GCM_TAG_LENGTH * 8, IV);
+
+        // Initialize Cipher for DECRYPT_MODE
+        cipher.init(Cipher.DECRYPT_MODE, keySpec, gcmParameterSpec);
+
+        // Perform Decryption
+        byte[] decryptedText = cipher.doFinal(Base64.getDecoder().decode(texto));
+
+        return new String(decryptedText);
     }
+
 }
--- chatbot-backend-web/src/test/java/co/com/santander/chatbot/backend/web/common/utilities/SecurityUtilitiesTest.java
+++ chatbot-backend-web/src/test/java/co/com/santander/chatbot/backend/web/common/utilities/SecurityUtilitiesTest.java
@@ -1,40 +1,24 @@
 package co.com.santander.chatbot.backend.web.common.utilities;
 
-import co.com.santander.chatbot.backend.web.common.utilities.SecurityUtilities;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import java.io.UnsupportedEncodingException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 
 @SpringBootTest
 public class SecurityUtilitiesTest {
 
-    private SecurityUtilities securityUtilities;
-
-    @Before
-    public void setUp() {
-        securityUtilities = new SecurityUtilities();
-    }
 
     @Test
-    public void testEncriptarSuccess() throws NoSuchPaddingException, UnsupportedEncodingException, IllegalBlockSizeException, BadPaddingException, NoSuchAlgorithmException, InvalidKeyException {
-        String encriptado = securityUtilities.encriptar("12345678AAAA");
+    public void testEncriptarSuccess() throws Exception {
+        String encriptado = SecurityUtilities.encriptar("12345678AAAA");
         Assert.assertNotNull(encriptado);
     }
 
     @Test
-    public void testDesencriptarSuccess() throws NoSuchPaddingException, UnsupportedEncodingException, IllegalBlockSizeException, BadPaddingException, NoSuchAlgorithmException, InvalidKeyException {
-        String desencriptado = securityUtilities.desencriptar("WhMHuAOSLgKlidOUbVwnng==");
+    public void testDesencriptarSuccess() throws Exception {
+        String desencriptado = SecurityUtilities.desencriptar("kcZsJENvAG1YUB7AZpHm5hhs2L4ZABjKyQD0IA==");
         Assert.assertNotNull(desencriptado);
 
     }
-
 
 }
