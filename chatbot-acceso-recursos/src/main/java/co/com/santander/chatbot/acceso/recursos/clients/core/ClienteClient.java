6feaf9625c9d495bc82e0c22a3ecb9aedba041d6
Jesus Sierra <jnsierrac@gmail.com>
2020-04-20 21:49:29 -0500

    Ajustes para la creacion de la obtencion de creditos

--- chatbot-acceso-recursos/src/main/java/co/com/santander/chatbot/acceso/recursos/clients/core/ClienteClient.java
+++ chatbot-acceso-recursos/src/main/java/co/com/santander/chatbot/acceso/recursos/clients/core/ClienteClient.java
@@ -5,6 +5,7 @@ import co.com.santander.chatbot.domain.payload.accesodatos.ResponsePayload;
 import co.com.santander.chatbot.domain.payload.accesodatos.cliente.ClienteViewPayload;
 import feign.Param;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,6 @@ public interface ClienteClient {
     @PostMapping(value = "${clientes.url.consultaCliente}")
     public ResponseEntity<ResponsePayload> conusltarCliente(@RequestHeader(AUTH_TOKEN) String bearerToken, ClientePayload cliente);
 
-    @GetMapping(value = "${clientes.url.consultaByTel}?telefono={telefono}")
+    @GetMapping(value = "${clientes.url.consultaCliente}?telefono=3005632010", produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<List<ClienteViewPayload>> getClientsByTel(@RequestHeader(AUTH_TOKEN) String bearerToken, @Param(value = "telefono") String telefono);
 }
--- chatbot-backend-web/src/main/java/co/com/santander/chatbot/backend/web/controller/ObtenerCreditosUsuarioController.java
+++ chatbot-backend-web/src/main/java/co/com/santander/chatbot/backend/web/controller/ObtenerCreditosUsuarioController.java
@@ -1,22 +1,33 @@
 package co.com.santander.chatbot.backend.web.controller;
 
+import co.com.santander.chatbot.backend.web.service.ClienteService;
 import co.com.santander.chatbot.domain.payload.service.obtenercreditos.CreditosUsuarioPayload;
 import co.com.santander.chatbot.domain.payload.service.obtenercreditos.ResponseObtenerCreditosPayload;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("v1/obtenerCreditosUsuario")
 public class ObtenerCreditosUsuarioController {
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    private final ClienteService clienteService;
+
+    @Autowired
+    public ObtenerCreditosUsuarioController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+
+    @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ResponseObtenerCreditosPayload> obtener(@RequestHeader("Authorization") String bearerToken, @RequestBody CreditosUsuarioPayload credito) {
-        return new ResponseEntity<>(ResponseObtenerCreditosPayload.builder()
-                .resultadoConsulta(Boolean.TRUE)
-                .idRespuesta(Long.valueOf("1"))
-                .descripcionRespuesta("Servicio consumido de forma exitosa")
-                .build(), HttpStatus.OK);
+        Optional<ResponseObtenerCreditosPayload> response = clienteService.obtenerCreditos(bearerToken, credito.getTelefono());
+        if (response.isPresent()) {
+            return new ResponseEntity<>(response.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
