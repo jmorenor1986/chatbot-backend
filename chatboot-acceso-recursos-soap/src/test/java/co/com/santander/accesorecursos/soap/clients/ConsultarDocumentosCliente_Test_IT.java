a0a69d16eaf623a313a73cc1af57db64c062d003
jmorenor1986 <johnmorenoing@gmail.com>
2020-05-12 21:23:48 +0200

    Test controller documentos

--- chatboot-acceso-recursos-soap/src/test/java/co/com/santander/accesorecursos/soap/clients/ConsultarDocumentosCliente_Test_IT.java
+++ chatboot-acceso-recursos-soap/src/test/java/co/com/santander/accesorecursos/soap/clients/ConsultarDocumentosCliente_Test_IT.java
@@ -1,11 +1,15 @@
 package co.com.santander.accesorecursos.soap.clients;
 
 import co.com.santander.accesorecursos.soap.common.exception.BusinessException;
+import co.com.santander.accesorecursos.soap.resources.documentos.ConsultarDocumentosResponse;
+import co.com.santander.chatbot.domain.payload.enviarextracto.ConsultarDocumentoPayload;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -13,9 +17,17 @@ public class ConsultarDocumentosCliente_Test_IT {
     @Autowired
     private DocumentosCliente documentosCliente;
 
+
     @Test(expected = BusinessException.class)
     public void testConsultarDocumentosError() {
-
+        List<ConsultarDocumentosResponse> result = documentosCliente.consultarDocumentos(ConsultarDocumentoPayload.builder()
+                .docId("1")
+                .fechaFin("")
+                .fechaIni("1212")
+                .folder("1")
+                .formatoConsulta("1")
+                .valorllave("1")
+                .build());
     }
 
 
--- chatboot-acceso-recursos-soap/src/test/java/co/com/santander/accesorecursos/soap/controller/DocumentosControllerTest.java
+++ chatboot-acceso-recursos-soap/src/test/java/co/com/santander/accesorecursos/soap/controller/DocumentosControllerTest.java
@@ -1,10 +1,38 @@
 package co.com.santander.accesorecursos.soap.controller;
 
+import co.com.santander.accesorecursos.soap.resources.documentos.ConsultarDocumentosResponse;
+import co.com.santander.accesorecursos.soap.service.DocumentosService;
+import co.com.santander.chatbot.domain.payload.enviarextracto.ConsultarDocumentoPayload;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 public class DocumentosControllerTest {
 
-
+    @Mock
+    private DocumentosService documentosService;
     private DocumentosController documentosController;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+        documentosController = new DocumentosController(documentosService);
+    }
+
+    @Test
+    public void testConsultaDocumentosSuccess() {
+        Mockito.when(documentosService.consultarDocumentos(Mockito.any())).thenReturn(new ArrayList<>());
+        ResponseEntity<List<ConsultarDocumentosResponse>> result = documentosController.consultaDocumentos(ConsultarDocumentoPayload.builder().build());
+        Assert.assertNotNull(result);
+        Assert.assertEquals(200, result.getStatusCodeValue());
+    }
 }
