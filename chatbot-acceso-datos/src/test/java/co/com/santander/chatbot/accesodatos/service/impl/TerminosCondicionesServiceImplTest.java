85fd72c77a4c10a178e0e6ad03022a961d715a30
Jesus Sierra <jnsierrac@gmail.com>
2020-05-17 21:15:01 -0500

    Modificaciones para el test

--- chatbot-acceso-datos/src/test/java/co/com/santander/chatbot/accesodatos/service/impl/TerminosCondicionesServiceImplTest.java
+++ chatbot-acceso-datos/src/test/java/co/com/santander/chatbot/accesodatos/service/impl/TerminosCondicionesServiceImplTest.java
@@ -3,9 +3,11 @@ package co.com.santander.chatbot.accesodatos.service.impl;
 import co.com.santander.chatbot.accesodatos.entity.TerminosCondiciones;
 import co.com.santander.chatbot.accesodatos.repository.TerminosCondicionesRepository;
 import co.com.santander.chatbot.accesodatos.service.TerminosCondicionesService;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -18,9 +20,26 @@ public  class TerminosCondicionesServiceImplTest {
     private TerminosCondicionesService terminosCondicionesService;
     @Mock
     private TerminosCondicionesRepository terminosCondicionesRepository;
+
     @Before
     public void setUp(){
         MockitoAnnotations.initMocks(this);
         terminosCondicionesService = new TerminosCondicionesServiceImpl(terminosCondicionesRepository);
     }
+
+    @Test
+    public void testSave(){
+        TerminosCondiciones terminosResponse = TerminosCondiciones.builder()
+                .operacion(1L)
+                .horaEnviadoTeminos(new Date())
+                .telefono(3229032614L)
+                .id(1L)
+                .build();
+        Mockito.doReturn(terminosResponse).when(terminosCondicionesRepository).save(Mockito.any());
+
+        Optional<TerminosCondiciones> response = terminosCondicionesService.save(terminosResponse);
+        Assert.assertNotNull(response);
+        Assert.assertTrue(response.isPresent());
+    }
+
 }
